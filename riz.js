a683674f897bd27428fe1292ce7d03c58caf0675
H4M1D1 <666764@pdsb.net>
2019-10-09 10:01:19 -0400

    awdada

--- riz.js
+++ riz.js
@@ -18,3 +18,34 @@ function guess_number(){
     }
   }
 }
+
+function centguess(){
+var x = prompt("Type in any year, my website will tell you what century that year belongs to!");
+var y =(x/100)
+var z =(y + 1)
+    alert("This year is in the" + " " + Math.floor(z) + "th Century")
+}
+
+function assignment(){
+  var secret = "Sarah";
+  var guess;
+  while(guess != secret){
+    guess = prompt("What is your first name?");
+
+    if (guess == secret){
+      alert("Sorry Ms.Pais, this assignment is still under construction.");
+    }
+    else if(guess != secret){
+      alert("Why are you trying to snoop my assignments? LEAVEEEEEEE!!!!!");
+    }
+    else{
+      alert("That's not a name silly!");
+    }
+  }
+}
+
+function angle(){
+var x = prompt("My website will tell you the value of an angle if you provide it with the other 2 angles! Enter the first angle value for a triangle!");
+var y = prompt("Enter the second angle... ");
+      alert(180 - x - y + " " + "degrees is the last angle of that triangle!");
+}
