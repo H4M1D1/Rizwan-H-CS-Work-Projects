35fc3d6ca48dcb4536380602e50e1473d36228d1
H4M1D1 <54900151+H4M1D1@users.noreply.github.com>
2019-10-31 20:36:57 -0400

    Update riz.js

--- riz.js
+++ riz.js
@@ -50,6 +50,85 @@ var y = prompt("Enter the second angle... ");
       alert(180 - x - y + " " + "degrees is the last angle of that triangle!");
 }
 
+function getHistory(){
+  return document.getElementById("history-value").innerText;
+}
+function printHistory(num){
+  document.getElementById("history-value").innerText=num;
+}
+function getOutput(){
+  return document.getElementById("output-value").innerText;
+}
+function printOutput(num){
+  if(num==""){
+    document.getElementById("output-value").innerText=num;
+  }
+  else{
+    document.getElementById("output-value").innerText=getFormattedNumber(num);
+  } 
+}
+function getFormattedNumber(num){
+  if(num=="-"){
+    return "";
+  }
+  var n = Number(num);
+  var value = n.toLocaleString("en");
+  return value;
+}
+function reverseNumberFormat(num){
+  return Number(num.replace(/,/g,''));
+}
+var operator = document.getElementsByClassName("operator");
+for(var i =0;i<operator.length;i++){
+  operator[i].addEventListener('click',function(){
+    if(this.id=="clear"){
+      printHistory("");
+      printOutput("");
+    }
+    else if(this.id=="backspace"){
+      var output=reverseNumberFormat(getOutput()).toString();
+      if(output){//if output has a value
+        output= output.substr(0,output.length-1);
+        printOutput(output);
+      }
+    }
+    else{
+      var output=getOutput();
+      var history=getHistory();
+      if(output==""&&history!=""){
+        if(isNaN(history[history.length-1])){
+          history= history.substr(0,history.length-1);
+        }
+      }
+      if(output!="" || history!=""){
+        output= output==""?output:reverseNumberFormat(output);
+        history=history+output;
+        if(this.id=="="){
+          var result=eval(history);
+          printOutput(result);
+          printHistory("");
+        }
+        else{
+          history=history+this.id;
+          printHistory(history);
+          printOutput("");
+        }
+      }
+    }
+    
+  });
+}
+var number = document.getElementsByClassName("number");
+for(var i =0;i<number.length;i++){
+  number[i].addEventListener('click',function(){
+    var output=reverseNumberFormat(getOutput());
+    if(output!=NaN){ //if output is a number
+      output=output+this.id;
+      printOutput(output);
+    }
+  });
+}
+
 function integer(){
   var x = 0 
   while (x <= 99){
@@ -65,20 +144,3 @@ function integer(){
       }
     }
   }
-
-function getHistory(){
-  return document.getElementById("history-value").innerText;
-}
-
-function printHistory(num){
-  return document.getElementById("history-value").innerText=num;
-}
-
-function getOutput(){
-  return document.getElementById("output-value").innerText;
-}
-
-function printOutput(num){
-  document.getELementById("output-value").innerText=num;
-}
-printOutput("9999")
