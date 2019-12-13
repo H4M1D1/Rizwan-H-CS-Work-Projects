a17dc1f7e629ae6a73ad8e7c2562d262d1a264b1
H4M1D1 <666764@pdsb.net>
2019-12-13 10:29:40 -0500

    dsadsad

--- riz.js
+++ riz.js
@@ -70,3 +70,25 @@ function sentlength(){
   var a = prompt("Enter a set of words, my website will let you know how many characters there are in that phrase!")
   alert(a.length);
 }
+
+function encrypt(){
+  var x = prompt("Enter any letter, word, or phrase!")
+  var y = x.toUpperCase()
+  var z = y.replace(/A/g, "q").replace(/B/g, "w").replace(/C/g, "e").replace(/D/g, "r").replace(/E/g, "t").replace(/F/g, "y").replace(/G/g, "u").replace(/H/g, "i").replace(/I/g, "o").replace(/J/g, "p").replace(/K/g, "a").replace(/L/g, "s").replace(/M/g, "d").replace(/N/g, "f").replace(/O/g, "g").replace(/P/g, "h").replace(/Q/g, "j").replace(/R/g, "k").replace(/S/g, "l").replace(/T/g, "z").replace(/U/g, "x").replace(/V/g, "c").replace(/W/g, "v").replace(/X/g, "b").replace(/Y/g, "n").replace(/Z/g, "m")
+  if (/[0-9]/.test(x)){
+    alert("No numbers, please!");
+  }else{
+    alert(z)
+  }
+}
+
+function decrypt(){
+  var p = prompt("Enter any letter, word, or phrase!")
+  var f = p.toUpperCase()
+  var d = f.replace(/Q/g, "a").replace(/W/g, "b").replace(/E/g, "c").replace(/R/g, "d").replace(/T/g, "e").replace(/Y/g, "f").replace(/U/g, "g").replace(/I/g, "h").replace(/O/g, "i").replace(/P/g, "j").replace(/A/g, "k").replace(/S/g, "l").replace(/D/g, "m").replace(/F/g, "n").replace(/G/g, "o").replace(/H/g, "p").replace(/J/g, "q").replace(/K/g, "r").replace(/L/g, "s").replace(/Z/g, "t").replace(/X/g, "u").replace(/C/g, "v").replace(/V/g, "w").replace(/B/g, "x").replace(/N/g, "y").replace(/M/g, "z")
+  if (/[0-9]/.test(p)){
+    alert("No numbers, please!");
+  }else{
+    alert(d)
+  }
+}
